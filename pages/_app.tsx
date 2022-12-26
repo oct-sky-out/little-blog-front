3be20201150b5f78c06677bbd9b8888e35f9f26d
oct-sky-out <kms3335k@naver.com>
2022-12-26 15:51:55 +0900

    메뉴 레이아웃 컴포넌트 공통화 1건 1. 메뉴 레이아웃 컴포넌트 공통화 2. 메인 페이지 초기화

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,15 +1,45 @@
 import '../styles/globals.css';
-import type { AppProps } from 'next/app';
+import type { AppContext, AppProps } from 'next/app';
 import CssBaseline from '@mui/material/CssBaseline';
 import { ThemeProvider } from 'next-themes';
+import Head from 'next/head';
+import { CategoryCollection } from '@/router/category/CategoryResponse';
+import axios from '@/router/util/axios';
+import MenuLayout from '@/components/menu/MenuLayout';
 
-export default function App({ Component, pageProps }: AppProps) {
+function MyApp({ Component, pageProps }: AppProps) {
+  const { categoryEntities, ...pagePropz } = pageProps;
+
   return (
     <>
       <ThemeProvider attribute="class">
         <CssBaseline />
-        <Component {...pageProps} />
+        <Head>
+          <title>oct-sky-out 블로그</title>
+          <meta name="description" content="oct-sky-out의 블로그입니다." />
+          <link rel="icon" href="/favicon.svg" />
+        </Head>
+        <MenuLayout categoryEntities={categoryEntities}>
+          <Component {...pagePropz} />
+        </MenuLayout>
       </ThemeProvider>
     </>
   );
 }
+
+MyApp.getInitialProps = async ({ Component, ctx }: AppContext) => {
+  let pageProps = {};
+
+  if (Component.getInitialProps) {
+    pageProps = await Component.getInitialProps(ctx);
+  }
+
+  const result = await axios.get<CategoryCollection>('/api/categories');
+  const categoryEntities = result.data;
+
+  pageProps = { ...pageProps, categoryEntities };
+
+  return { pageProps };
+};
+
+export default MyApp;
