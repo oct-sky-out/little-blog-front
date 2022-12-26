ee4ef0d47103abed64bdc3712ef0e26a68f48db8
oct-sky-out <kms3335k@naver.com>
2022-12-26 16:41:05 +0900

    레이아웃 padding 미디어쿼리 삭제

--- components/menu/MenuLayout.tsx
+++ components/menu/MenuLayout.tsx
@@ -9,7 +9,7 @@ export type MenuLayoutProps = {
 
 const MenuLayout = ({ categoryEntities, children }: MenuLayoutProps) => {
   return (
-    <div className="xl:w-4/5 xl:mx-auto xl:xl:my-0 xl:pt-2">
+    <div className="xl:w-4/5 xl:mx-auto xl:xl:my-0 pt-2">
       <CategoryComponent categoryEntities={categoryEntities} />
       {children}
     </div>
